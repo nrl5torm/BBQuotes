d1c1a26a91b8b23c8ed1d8505053e619c21a990a
Olivier Sbg <oliv@macOliv.local>
2026-09-07 14:35:06 +0200

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Olivier Sbg on 07/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
